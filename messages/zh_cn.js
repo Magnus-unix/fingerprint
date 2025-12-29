b4dfbe9ebfc3ca083f69a18be347c10a487caf25
Magnus-unix <Magnus-unix@users.noreply.github.com>
2025-12-29 09:36:29 +0000

    deploy: d9ec34563335d38b52534dc4b1ddd21718c3dc2e

---
+++ messages/zh_cn.js
@@ -0,0 +1,37 @@
+if (!window.messages) {
+  window.messages = {};
+}
+
+window.messages.zh_cn = {
+  'language': '中文 | <a href="javascript:;" onclick="alert(\'Donate the author $50 for english version\')">EN</a>',
+  'systemname': 'SSO统一身份认证',
+  'login.type.password': '密码登录',
+  'login.type.sms': '短信登录',
+  'login.type.qrcode': '扫码登录',
+  'login.form.schoolid.title': '用户名',
+  'login.form.schoolid.placeholder': '请输入用户名',
+  'login.form.password.title': '密码',
+  'login.form.password.placeholder': '请输入密码',
+  'login.form.code.title': '验证码',
+  'login.form.code.placeholder': '请输入短信验证码',
+  'login.form.code.get': '获取验证码',
+  'login.form.code.errorsend': '发送失败。',
+  'login.form.code.notfound': '用户或手机号不存在。',
+  'login.form.code.threshold': '已达到今日发送上限。',
+  'login.form.captcha.placeholder': '请输入验证码',
+  'login.form.qrcode.tips': '二维码已失效',
+  'login.form.qrcode.text1': '请使用微信扫描二维码登录',
+  'login.form.qrcode.text2': 'SSO统一认证',
+  'login.form.btn.login': '登录',
+  'login.form.btn.refresh': '请点击刷新',
+  'login.form.error': '用户名或密码错误。',
+  'login.form.errorwithcode': '用户名或密码或验证码错误。',
+  'link.forgetpass': '忘记密码',
+  'link.signin': '立即注册',
+  'link.usercenter': '个人中心',
+  'link.changeSms': '用短信验证码登录',
+  'link.changePas': '用账号密码登录',
+  'copyright.icp': ' Cyber2077 © 2077 CVE-9999-00000',
+  'copyright.support': '技术支持：Network Center 8888-888888',
+  'copyright.browser': '浏览器支持：Chrome, Firefox, IE (8.0+)'
+}
